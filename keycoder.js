32bbacde9209d9d85bec23aaefeeffe8f8d13de6
Tom Hynard <tom@t-bone.com.au>
2015-06-02 15:43:09 +1000

    Adding styles for keys Adding code output

--- keycoder.js
+++ keycoder.js
@@ -3,9 +3,28 @@
 	var display = document.getElementById('display');
 	
 	var displayKeycode = function(event) {
+	
 		event.preventDefault();
 		
-		display.innerHTML = event.keyCode;
+		display.classList.remove('placeholder');
+		
+		var displayHTML = '';
+		
+		if(event.altKey && event.keyIdentifier !== 'Alt') {
+			displayHTML += '<span class="key">Alt</span> + ';
+		}
+		
+		if(event.ctrlKey && event.keyIdentifier !== 'Ctrl') {
+			displayHTML += '<span class="key">Ctrl</span> + ';
+		}
+		
+		if(event.shiftKey && event.keyIdentifier !== 'Shift') {
+			displayHTML += '<span class="key">Shift</span> + ';
+		}
+		
+		displayHTML += '<span class="key">' + event.keyIdentifier.replace('U+', '&#x') + '</span> = ' + event.keyCode;
+		
+		display.innerHTML = displayHTML;
 	};
 	
 	document.addEventListener('keydown', displayKeycode);
